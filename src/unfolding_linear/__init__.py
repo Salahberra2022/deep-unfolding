6d00ef69a71601c354f329005df3874264370d62
Nuno Fachada <nuno.fachada@ulusofona.pt>
2024-06-11 19:14:31 +0100

    Proper package find and export

--- src/unfolding_linear/__init__.py
+++ src/unfolding_linear/__init__.py
@@ -2,15 +2,27 @@
 # Distributed under the the GNU General Public License (See accompanying file LICENSE or copy
 # at https://www.gnu.org/licenses/)
 
-__author__ = "Salah Berra and contributors"
-__copyright__ = "Copyright 2022-2023"
-__license__ = "GNU GPL"
-__version__ = "0.1.0"
-__maintainer__ = "Salah Berra / Mohamed Nennouche"
-__email__ = "."
-__status__ = "dev"
 
-# Note that each Alghorithm import from method module and run in the main code Iterative.
-from .methods import *
-from .train_methods import *
-from .utils import *
+from unfolding_linear.methods import (
+    AOR,
+    AOR_CHEBY,
+    base_model,
+    GS,
+    Jacobi,
+    model_iterations,
+    RI,
+    SOR,
+    SOR_CHEBY,
+)
+from unfolding_linear.train_methods import (
+    AORNet,
+    evaluate_model,
+    RINet,
+    SORNet,
+    SOR_CHEBY_Net,
+    train_model,
+)
+from unfolding_linear.utils import (
+    decompose_matrix,
+    generate_A_H_sol,
+)
