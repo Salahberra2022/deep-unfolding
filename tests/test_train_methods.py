048a91305366d8eb0b3a2b66bd23a29174fb76b8
Nuno Fachada <nuno.fachada@ulusofona.pt>
2024-06-12 21:02:07 +0100

    Simplify imports in tests

--- tests/test_train_methods.py
+++ tests/test_train_methods.py
@@ -1,8 +1,10 @@
 import pytest
 import torch
 import torch.nn as nn
-from unfolding_linear.train_methods import train_model, evaluate_model, SORNet, SOR_CHEBY_Net, AORNet, RINet 
-from unfolding_linear.utils import device, generate_A_H_sol  
+from unfolding_linear import (
+    train_model, evaluate_model, SORNet, SOR_CHEBY_Net, AORNet, RINet,
+    device, generate_A_H_sol
+)
 
 # Fixture pour générer les matrices et tensors nécessaires
 @pytest.fixture
@@ -16,7 +18,7 @@ def generate_matrices():
 def test_SORNet_initialization(generate_matrices):
     A, H, y, n, m, bs, solution = generate_matrices
     model = SORNet(A, H, bs, y, device=device)
-    
+
     assert model.A.shape == (n, n), "Attribute A should have the correct shape"
     assert model.H.shape == (n, m), "Attribute H should have the correct shape"
     assert model.D.shape == (n, n), "Attribute D should have the correct shape"
@@ -30,9 +32,9 @@ def test_SORNet_initialization(generate_matrices):
 def test_SORNet_forward(generate_matrices):
     A, H, y, n, m, bs, solution = generate_matrices
     model = SORNet(A, H, bs, y, device=device)
-    
+
     s, traj = model.forward(num_itr=3)
-    
+
     assert s.shape == (bs, n), "Output tensor should have the correct shape"
     assert len(traj) == 4, "Trajectory should contain num_itr + 1 elements"
     assert all(t.shape == (bs, n) for t in traj), "Each element in the trajectory should have the correct shape"
@@ -42,7 +44,7 @@ def test_SOR_CHEBY_Net_initialization(generate_matrices):
     num_itr = 25
     A, H, y, n, m, bs, solution = generate_matrices
     model = SOR_CHEBY_Net(num_itr, A, H, bs, y, device=device)
-    
+
     assert model.A.shape == (n, n), "Attribute A should have the correct shape"
     assert model.H.shape == (n, m), "Attribute H should have the correct shape"
     assert model.D.shape == (n, n), "Attribute D should have the correct shape"
@@ -61,9 +63,9 @@ def test_SOR_CHEBY_Net_forward(generate_matrices):
     num_itr = 3
     A, H, y, n, m, bs, solution = generate_matrices
     model = SOR_CHEBY_Net(num_itr, A, H, bs, y, device=device)
-    
+
     s, traj = model.forward(num_itr=num_itr)
-    
+
     assert s.shape == (bs, n), "Output tensor should have the correct shape"
     assert len(traj) == num_itr + 1, "Trajectory should contain num_itr + 1 elements"
     assert all(t.shape == (bs, n) for t in traj), "Each element in the trajectory should have the correct shape"
@@ -73,7 +75,7 @@ def test_SOR_CHEBY_Net_forward(generate_matrices):
 def test_AORNet_initialization(generate_matrices):
     A, H, y, n, m, bs, solution = generate_matrices
     model = AORNet(A, H, bs, y, device=device)
-    
+
     assert model.A.shape == (n, n), "Attribute A should have the correct shape"
     assert model.H.shape == (n, m), "Attribute H should have the correct shape"
     assert model.D.shape == (n, n), "Attribute D should have the correct shape"
@@ -88,9 +90,9 @@ def test_AORNet_initialization(generate_matrices):
 def test_AORNet_forward(generate_matrices):
     A, H, y, n, m, bs, solution = generate_matrices
     model = AORNet(A, H, bs, y, device=device)
-    
+
     s, traj = model.forward(num_itr=3)
-    
+
     assert s.shape == (bs, n), "Output tensor should have the correct shape"
     assert len(traj) == 4, "Trajectory should contain num_itr + 1 elements"
     assert all(t.shape == (bs, n) for t in traj), "Each element in the trajectory should have the correct shape"
@@ -99,7 +101,7 @@ def test_AORNet_forward(generate_matrices):
 def test_RINet_initialization(generate_matrices):
     A, H, y, n, m, bs, solution = generate_matrices
     model = RINet(A, H, bs, y, device=device)
-    
+
     assert model.A.shape == (n, n), "Attribute A should have the correct shape"
     assert model.H.shape == (n, m), "Attribute H should have the correct shape"
     assert model.D.shape == (n, n), "Attribute D should have the correct shape"
@@ -113,9 +115,9 @@ def test_RINet_initialization(generate_matrices):
 def test_RINet_forward(generate_matrices):
     A, H, y, n, m, bs, solution = generate_matrices
     model = RINet(A, H, bs, y, device=device)
-    
+
     s, traj = model.forward(num_itr=3)
-    
+
     assert s.shape == (bs, n), "Output tensor should have the correct shape"
     assert len(traj) == 4, "Trajectory should contain num_itr + 1 elements"
     assert all(t.shape == (bs, n) for t in traj), "Each element in the trajectory should have the correct shape"
@@ -130,16 +132,16 @@ def test_train_model(generate_matrices):
     loss_func = nn.MSELoss()
 
     trained_model, loss_gen = train_model(model, optimizer, loss_func, solution, total_itr=3, num_batch=5)
-    
+
     assert isinstance(trained_model, SORNet), "Returned model should be of type SORNet"
     assert len(loss_gen) == 3, "Length of loss_gen should be equal to total_itr"
 
 def test_evaluate_model(generate_matrices):
     A, H, y, n, m, bs, solution = generate_matrices
     model = SORNet(A, H, bs, y, device=device)
-    
+
     norm_list = evaluate_model(model, solution, n, bs=bs, total_itr=3, device=device)
-    
+
     assert len(norm_list) == 4, "Length of norm_list should be equal to total_itr + 1"
     assert all(isinstance(err, float) for err in norm_list), "All elements in norm_list should be floats"
 
